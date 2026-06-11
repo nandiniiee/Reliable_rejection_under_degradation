c05a1925ce2a158216488be0e89418b93ebc9db8
nandiniee <jain.nandini.squash@gmail.com>
2026-06-12 00:01:58 +0530

    code for generating noise added

--- project_codes/verification_based/noise.py
+++ project_codes/verification_based/noise.py
@@ -8,7 +8,6 @@ input_folder = "probe_images/cleanImages"
 # Output folders
 noise1_folder = "probe_images/noise_level1"
 noise2_folder = "probe_images/noise_level2"
-
 os.makedirs(noise1_folder, exist_ok=True)
 os.makedirs(noise2_folder, exist_ok=True)
 
@@ -20,20 +19,13 @@ def add_gaussian_noise(image, sigma):
     """
 
     noise = np.random.normal(loc=0, scale=sigma, size=image.shape)
-
     noisy_image = image.astype(np.float32) + noise
-
     noisy_image = np.clip(noisy_image, 0, 255).astype(np.uint8)
-
     return noisy_image
 
-
 for filename in os.listdir(input_folder):
-
     image_path = os.path.join(input_folder, filename)
-
     img = cv2.imread(image_path)
-
     if img is None:
         continue
 
@@ -44,9 +36,7 @@ for filename in os.listdir(input_folder):
     noisy2 = add_gaussian_noise(img, sigma=30)
 
     cv2.imwrite(os.path.join(noise1_folder, filename), noisy1)
-
     cv2.imwrite(os.path.join(noise2_folder, filename), noisy2)
-
     print(f"Processed {filename}")
 
 print("\nNoise generation completed.")
